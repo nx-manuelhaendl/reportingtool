086b6f56169505d782a7d32f1618efb3181accca
Manuel Händl <manuel.haendl@nexum.de>
2020-05-02 22:28:47 +0200

    004 - Bug Fixes Starter File

--- getData.php
+++ getData.php
@@ -3,20 +3,8 @@
 // This is just an example of reading server side data and sending it to the client.
 // It reads a json formatted text file and outputs it.
 
-$json = '{
-    "cols": [
-    {"id":"","label":"Test","pattern":"","type":"string"},
-    {"id":"","label":"Test2","pattern":"","type":"number"}
-    ],
-    "rows": [
-    {"c":[{"v":"Open","f":null},{"v":3,"f":null}]},
-    {"c":[{"v":"Click","f":null},{"v":1,"f":null}]},
-    {"c":[{"v":"Bounce","f":null},{"v":1,"f":null}]},
-    {"c":[{"v":"Job","f":null},{"v":1,"f":null}]},
-    {"c":[{"v":"Unsubscribes","f":null},{"v":2,"f":null}]}
-    ]
-}';
-echo json_decode($json);
+$string = file_get_contents("data.json");
+echo $string;;
 
 // Instead you can query your database and parse into JSON etc etc
 
